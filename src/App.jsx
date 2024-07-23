dff2cb17225bb5865e9b88fe411bcd123a0a4fd3
Shazib Siddique <siddiqueshazib4@gmail.com>
2024-07-23 12:56:20 -0400

    error handling and create service

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,39 @@
 // src/App.jsx
 
+import { useState, useEffect } from 'react';
+
+import * as petService from './services/petService';
+
+// src/App.jsx
+
 const App = () => {
+  const [petList, setPetList] = useState([]);
+
+  // src/App.jsx
+
+useEffect(() => {
+  const fetchPets = async () => {
+    try {
+      const pets = await petService.index();
+      // Don't forget the pass the error object to the new Error
+      if (pets.error) {
+        throw new Error(pets.error);
+      }
+
+      setPetList(pets);
+    } catch (error) {
+      // Log the error object
+      console.log(error);
+    }
+  };
+  fetchPets();
+}, []);
+
+
   return <h1>Hello world!</h1>;
 };
+// READ - GET - /pets
+
+
 
 export default App;
